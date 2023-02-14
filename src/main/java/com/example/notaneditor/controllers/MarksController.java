c98f73f0ada4349de7aaeb8a0d73b4a608ebb2e4
Carlos Triana <uo283428@uniovi.es>
2023-02-14 21:41:15 +0100

    SDI-2223-420-2.2-Beans, servicios e inyección de dependencias.

--- src/main/java/com/example/notaneditor/controllers/MarksController.java
+++ src/main/java/com/example/notaneditor/controllers/MarksController.java
@@ -1,31 +1,35 @@
 package com.example.notaneditor.controllers;
 
 import com.example.notaneditor.entities.Mark;
+import com.example.notaneditor.services.MarksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MarksController {
 
+    @Autowired //Inyectar el servicio
+    private MarksService marksService;
+
+
     @RequestMapping("/mark/list")
     public String getList() {
-        return "Getting List";
+        return marksService.getMarks().toString();
     }
     @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
     public String setMark(@ModelAttribute Mark mark) {
-        return "added: " + mark.getDescription()
-                + " with score : " + mark.getScore()
-                + " id: " + mark.getId();
+        marksService.addMark(mark);
+        return "OK";
     }
 
-    /*
-    @RequestMapping("/mark/details")
-    public String getDetail(@RequestParam Long id) {
-        return "Getting Details =>" + id;
-    }
-     */
     @RequestMapping("/mark/details/{id}")
     public String getDetail(@PathVariable Long id) {
-        return "Getting Details =>" + id;
+        return marksService.getMark(id).toString();
     }
 
+    @RequestMapping("/mark/delete/{id}")
+    public String deleteMark(@PathVariable Long id){
+        marksService.deleteMark(id);
+        return "OK";
+    }
 }
