4fcb096d6d28e9ccc9f0d9bf72db293f41f905b1
Carlos Triana <uo283428@uniovi.es>
2023-02-14 20:42:38 +0100

    SDI-2223-420-2.1-Create Spring project

---
+++ src/main/java/com/example/notaneditor/Sdi2223410SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.notaneditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223410SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223410SpringApplication.class, args);
+    }
+
+}
