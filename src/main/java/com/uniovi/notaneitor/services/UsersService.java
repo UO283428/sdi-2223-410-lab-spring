7a5b2a3d245d4f4720ec730e2f8240e96eae6662
Carlos Triana <uo283428@uniovi.es>
2023-02-20 18:54:12 +0100

    SDI-2223-420-3.4-Autenticación y control de acceso.

--- src/main/java/com/uniovi/notaneitor/services/UsersService.java
+++ src/main/java/com/uniovi/notaneitor/services/UsersService.java
@@ -7,10 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.uniovi.notaneitor.entities.*;
 import com.uniovi.notaneitor.services.UsersService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Service
 public class UsersService {
     @Autowired
     private UsersRepository usersRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @PostConstruct
     public void init() {
     }
@@ -23,7 +27,11 @@ public class UsersService {
         return usersRepository.findById(id).get();
     }
     public void addUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         usersRepository.save(user);
+    }
+    public User getUserByDni(String dni) {
+        return usersRepository.findByDni(dni);
     }
     public void deleteUser(Long id) {
         usersRepository.deleteById(id);
